217b402bee77c64c59abc5e60fadb2f300f689ef
dazz <annejulia@gmail.com>
2022-12-11 12:11:46 +0100

    feat(day04): day 4 completed

---
+++ day04/part1.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+use function Crell\fp\amap;
+use function Crell\fp\pipe;
+use function Crell\fp\explode;
+use function Crell\fp\afilter;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$inputFile = __DIR__ . '/input.txt';
+
+function lines(string $file): iterable
+{
+    $fp = fopen($file, 'rb');
+    while ($line = fgets($fp)) {
+        yield trim($line);
+    }
+    fclose($fp);
+}
+
+function fullyContains(array $pairs): bool
+{
+    $first = explode('-')($pairs[0]);
+    $second = explode('-')($pairs[1]);
+
+    if (
+        ($first[0] >= $second[0] && $first[1] <= $second[1]) || // first is in second
+        ($first[0] <= $second[0] && $first[1] >= $second[1])    // second is in first
+    ) {
+        return true;
+    }
+    return false;
+}
+
+
+$result = pipe($inputFile,
+    lines(...),
+    amap(explode(',')),
+    afilter(overlap(...)),
+    count(...),
+);
+
+//print_r($result);
+print $result . PHP_EOL;
